51a3a53393d9e23fb1a52aaf6cbeb5b6de9e8645
Fruki <lennartgudd@fh-muenster.de>
2013-06-20 14:23:36 +0200

    Login LudoSystem

---
+++ LudoSystem/ejbModule/com/example/SystemResponse/LoginResponse.java
@@ -0,0 +1,18 @@
+package com.example.SystemResponse;
+
+public class LoginResponse {
+	
+	private int sessionId;
+	
+	public LoginResponse() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public int getSessionId(){
+		return sessionId;
+	}
+	
+	public void setSessionId(int sessionId){
+		this.sessionId = sessionId;
+	}
+}
